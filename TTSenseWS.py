6c2c9c39a3133bc814fcd6514790879cbbbfe204
SomeKindaHackrMan <155858992+SomeKindaHackrMan@users.noreply.github.com>
2024-07-25 12:04:01 -0700

    Update TTSenseWS.py

--- TTSenseWS.py
+++ TTSenseWS.py
@@ -184,5 +184,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         #create_user('username', 'password')
-  
+        #add_verified_hash('hash')
+        #add_malicious_hash('hash')
     app.run(debug=True)
